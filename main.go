bef553365245c674b8047ae72a3f8e0c0a69b304
Soroush Beigi <sbeygi712@gmail.com>
2025-07-04 16:41:52 +0330

    feat(server): Implemented http server and simple health check method

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
-import "github.com/SoroushBeigi/workout-go/internal/app"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/SoroushBeigi/workout-go/internal/app"
+)
 
 func main() {
 	app, err := app.NewApplication()
-	if err!=nil{
+	if err != nil {
 		panic(err)
 	}
 	app.Logger.Println("running")
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "\nStatus is available\n")
 }
