6e5dd5f4c2c938905074c9cae3acc33dd7932f2b
Soroush Beigi <sbeygi712@gmail.com>
2025-07-31 11:13:17 +0330

    feat(store): added user store

---
+++ internal/store/user_store.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+)
+
+type password struct {
+	plainText string
+	hash      []byte
+}
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash password  `json:"-"`
+	Bio          string    `json:"bio"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type PostgresUserStore struct {
+	db *sql.DB
+}
+
+func NewPostgresUserStore(db *sql.DB) *PostgresUserStore {
+	return &PostgresUserStore{db: db}
+}
+
+type UserStore interface {
+	CreateUser(*User) error
+	GetUserByUsername(string) (*User, error)
+	UpdateUser(*User) error
+}
